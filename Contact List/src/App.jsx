92ff59e681c2fac6f789ec45c9410ad382cb8026
Matthew De Andrade <fayrfax@gmail.com>
2024-10-03 22:38:16 -0400

    Added readable contact list.

--- Contact List/src/App.jsx
+++ Contact List/src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import { ContactList } from "./components/ContactList.jsx";
 
-function App() {
-  return <></>;
+export function App() {
+  const dummyContacts = [
+    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+  ];
+  const [contacts, setContacts] = useState(dummyContacts);
+
+  console.log("Contacts: ", contacts);
+  return (
+    <>
+      <ContactList contacts={contacts} setContacts={setContacts} />
+    </>
+  );
 }
 
 export default App;
